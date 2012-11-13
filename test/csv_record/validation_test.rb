724928a8d9272e28e890f0d5b93bb9d8815d1fde
urieljuliatti <uriel.juliattivalle@gmail.com>
2012-11-13 12:52:15 -0200

    More test assertions and contexts

--- test/csv_record/validation_test.rb
+++ test/csv_record/validation_test.rb
@@ -9,11 +9,20 @@ describe CsvRecord::Validation do
     it ('responds to validates_presence_of') { Jedi.must_respond_to :validates_presence_of }
   end
 
-  describe 'validates_presence_of :name behavior' do
+  describe 'validates_presence_of :name and :age behavior' do
 
     it 'is not valid without name' do
       yoda.name = nil
       yoda.valid?.wont_equal true
+    end
+
+    it 'is not valid without age' do
+      yoda.age = nil
+      yoda.valid?.wont_equal true
+    end
+
+    it "is valid with all attributes filled" do
+      yoda.valid?.must_equal true
     end
 
   end
