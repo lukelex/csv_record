f77a4c544df3b2bad3d9244b76076d4ee03a2751
urieljuliatti <uriel.juliattivalle@gmail.com>
2012-11-13 14:31:54 -0200

    Removing unnecessary method

--- lib/csv_record/validation.rb
+++ lib/csv_record/validation.rb
@@ -3,16 +3,11 @@ module CsvRecord
   module Validation
     attr_reader :errors, :fields_to_validate
 
-    def __fields_to_validate_builder__(attr_names)
+    def __validates_presence_of__(*attr_names)
       @fields_to_validate = attr_names
     end
 
-    def __validates_presence_of__(*attr_names)
-      fields_to_validate_builder(attr_names)
-    end
-
     alias :validates_presence_of :__validates_presence_of__
-    alias :fields_to_validate_builder :__fields_to_validate_builder__
 
     module InstanceMethods
 
