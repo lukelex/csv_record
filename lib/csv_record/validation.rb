0bbaf305e5b2b3b4889f3e0f2f4489c8d4d0abab
urieljuliatti <uriel.juliattivalle@gmail.com>
2012-11-13 12:55:47 -0200

    Removing unnecessary methods

--- lib/csv_record/validation.rb
+++ lib/csv_record/validation.rb
@@ -16,10 +16,6 @@ module CsvRecord
 
     module InstanceMethods
 
-      def validate_each(attritbutes)
-        attritbutes.collect.none? { |attr| self.public_send(attr).nil? }
-      end
-
       def __valid__?
         validate_each(self.class.fields_to_validate)
       end
@@ -27,18 +23,11 @@ module CsvRecord
       alias :valid? :__valid__?
 
       private
-      # extract values from attributes in order to organize, ex: {:attribute => :name, :value => nil}
-      def extract_value(attr)
-        value = {}
-        value[attr] = self.public_send(attr)
-        value
+      def validate_each(attritbutes)
+        attritbutes.collect.none? { |attr| self.public_send(attr).nil? }
       end
+
     end
 
-    private
-    def _merge_attributes(attr_names)
-      options = attr_names.extract_options!
-      options.merge(:attributes => attr_names.flatten)
-    end
   end
 end
