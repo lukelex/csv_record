018a84b1ae9039113e3cbf5980ee607949584dec
Lukas Alexandre <lukasalexandre@me.com>
2013-02-10 07:43:58 -0300

    Removing unecessary code template on CustomizedClass

--- test/models/customized_class.rb
+++ test/models/customized_class.rb
@@ -2,14 +2,4 @@ class CustomizedClass
   include CsvRecord::Document
 
   store_as :modification
-
-  attr_accessor :name
-
-  def initialize(params={})
-    params.each do |key, value|
-      self.public_send("#{key}=", value)
-    end
-  end
-
-
 end
