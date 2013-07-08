0d9aa4f841477375368ba2c2f8416a695104d0ca
Lukas Alexandre <lukasalexandre@me.com>
2013-07-08 13:40:20 -0300

    Moving writer modules into a single file

--- lib/csv_record/csv_writers/writer.rb
+++
@@ -1,9 +0,0 @@
-require 'csv_record/csv_writers/instance_writer'
-require 'csv_record/csv_writers/class_writer'
-
-module CsvRecord::Writer
-  def self.included(receiver)
-    receiver.extend         ClassMethods
-    receiver.send :include, InstanceMethods
-  end
-end
--- lib/csv_record/writer.rb
+++ lib/csv_record/writer.rb
@@ -1,4 +1,55 @@
 module CsvRecord::Writer
+  module ClassMethods
+    def __create__(attributes={})
+      instance = self.build attributes
+      yield instance if block_given?
+      result = instance.save
+      instance
+    end
+
+    [:attr_accessor, :attr_writer].each do |custom_accessor|
+      define_method custom_accessor do |*attributes|
+        attributes.each do |attribute|
+          unless self.fields.has_name? attribute
+            self.fields << CsvRecord::Field.new(attribute)
+          end
+        end
+        super *attributes
+      end
+    end
+
+    def store_as(name)
+      @table_name = name.to_s.underscore.pluralize
+      redefine_database_location
+
+      @table_name
+    end
+
+    def table_name
+      @table_name ||= store_as name
+    end
+
+    def mapping(config=[])
+      config.each do |field, doppelganger|
+        unless self.fields.include? field
+          self.fields << (CsvRecord::Field.new field, doppelganger)
+        end
+      end
+    end
+
+    def redefine_database_location
+      if const_defined?('DATABASE_LOCATION') || const_defined?('DATABASE_LOCATION_TMP')
+        send :remove_const, 'DATABASE_LOCATION'
+        send :remove_const, 'DATABASE_LOCATION_TMP'
+      end
+
+      const_set 'DATABASE_LOCATION', "db/#{@table_name}.csv"
+      const_set 'DATABASE_LOCATION_TMP', "db/#{@table_name}_tmp.csv"
+    end
+
+    alias :create :__create__
+  end
+
   module InstanceMethods
     def self.included(receiver)
       receiver.send :attr_accessor, :id
@@ -82,5 +133,10 @@ module CsvRecord::Writer
     alias :update_attribute :__update_attribute__
     alias :destroy :__destroy__
     alias :update_attributes :__update_attributes__
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
   end
 end
